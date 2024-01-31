9267a90c29f46040a0abe588b5cedc5b5ad20029
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-31 15:51:53 +0000

    Fix task 1,2,3,4

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Define BasicCache class"""
 from collections import OrderedDict
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Define MRUCache class"""
 from collections import OrderedDict
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
