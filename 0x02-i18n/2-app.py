52c752f1c0d3f90f0747cfe333c57baf03ba1ff6
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-06 05:21:19 +0000

    Add task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Define a basic flask project"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
+# pylint: disable=E1101
 
 
 class Config:
@@ -18,11 +20,17 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Get the local from a webpage"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route("/")
 @app.route("/home")
 def home() -> str:
     """Define home route"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
